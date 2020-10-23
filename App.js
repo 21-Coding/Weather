52ef43dd807988ae378c6da3e6039cc5258580e4
Dom Youmans <ddyoumans@icloud.com>
2020-10-23 00:08:42 -0700

    add initial.

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+
 
 export default function App() {
+  useEffect(() => {
+    load()
+  }, [])
+
+  async function load() {
+    try {
+      let { status } = await Location.requestPermissionAsync()
+
+      if(status != 'granted'){
+        setErrorMessage('Acces to location to run the app')
+        return
+      }
+      const location = await Location.getCurrentPositionAsync()
+
+      const {latitude, longitude} = location.coords
+      alert(`Latitiude : $(latitude), Longtitude: $(longtitude)`)
+    }  catch (error) {
+
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Lance's Weather</Text>
       <StatusBar style="auto" />
     </View>
   );
